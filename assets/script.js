54346cd851a8bfacf09b3baec25c2e08e1466956
Theo Belland <theo.belland.pro@gmail.com>
2025-07-07 11:20:32 +0200

    Mise en place du slider, -> Fleche droite et gauche -> Création des bullets -> Création des slides

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,41 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let currentIndex = 0;
+
+const bannerImg = document.querySelector('.banner-img');
+const bannerText = document.querySelector('#banner p');
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const dotsContainer = document.querySelector('.dots');
+
+slides.forEach((_, index) => {
+	const dot = document.createElement('div');
+	dot.classList.add('dot');
+	if (index === currentIndex) {
+		dot.classList.add('dot_selected');
+	}
+	dotsContainer.appendChild(dot);
+});
+
+function updateBanner(index){
+	bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+	bannerText.innerHTML = slides[index].tagLine;
+	const allDots = document.querySelectorAll('.dot');
+	allDots.forEach((dot, i) => {
+		dot.classList.toggle('dot_selected', i == index);
+	});
+}
+
+
+arrowLeft.addEventListener('click', () => {
+	currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+	updateBanner(currentIndex);
+});
+
+arrowRight.addEventListener('click', () => {
+	currentIndex = (currentIndex + 1) % slides.length;
+	updateBanner(currentIndex);
+});
+
